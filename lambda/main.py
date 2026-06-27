abadd31f8a94a40f09e66fe09a7c0333e8e0d30d
Shayan <shayanshaterpoori@outlook.com>
2026-06-27 18:36:34 +0100

    add to the python code, implement a skeleton for the extraction stage.

--- lambda/main.py
+++ lambda/main.py
@@ -3,27 +3,39 @@ import json
 import boto3
 from datetime import date
 from dotenv import load_dotenv
+import os
+from pprint import pprint
+
 load_dotenv()
 
 def lambda_handler():
         # event, context):
     today = date.today().isoformat()
     
+
+    NASA_API_KEY = os.getenv("NASA_API_KEY")
     # 1. Call the API
     response = requests.get(
         "https://api.nasa.gov/neo/rest/v1/feed",
         params={
             "start_date": today,
             "end_date": today,
-            "api_key": "YOUR_KEY"  # store in AWS Secrets Manager
+            "api_key": NASA_API_KEY  # store in AWS Secrets Manager
         }
     )
     data = response.json()
+
     
+
     # 2. Store raw JSON to S3
     s3 = boto3.client("s3")
+    buckets = s3.list_buckets(
+        Prefix = "neo-data-lake-"
+    )
+    neo_bucket_name = buckets['Buckets'][0]['Name']
+    pprint(neo_bucket_name)
     s3.put_object(
-        Bucket="your-neo-raw-bucket",
+        Bucket=neo_bucket_name,
         Key=f"raw/{today}.json",
         Body=json.dumps(data)
     )
